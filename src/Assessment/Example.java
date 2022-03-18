1a4117c5de0dffc07d2bc98f7cb44a903014440c
Sambit Kumar Pradhan <48365115+psambitkumar@users.noreply.github.com>
2022-03-18 11:54:11 +0530

    New Commit on java 8 some features using like lamda expression

---
+++ src/Assessment/Example.java
@@ -0,0 +1,12 @@
+package Assessment;
+
+public class Example {
+    public static void main(String[] args) {
+        int x = 10;
+        String y = "10";
+        if(y.equals(x))
+            System.out.println("This is Equal");
+        else
+            System.out.println("Not Equal");
+    }
+}
--- src/Assessment/Q6.java
+++ src/Assessment/Q6.java
@@ -6,7 +6,7 @@ import java.util.Map;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
-class Employee2 {
+class  Employee2 {
     int empcode;
     String firstName;
     String lastName;
